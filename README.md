b739d3e084ba2b3223ec9be1262bde0e0feba721
Martín <magadiflo@gmail.com>
2022-05-31 20:02:16 -0500

    Inicio del proyecto - JSON Web Token (JWT) with Spring Security And Angular

---
+++ README.md
@@ -0,0 +1,5 @@
+# Información del Curso - Udemy
+
+<br>Curso: </b> JSON Web Token (JWT) with Spring Security And Angular
+<br>Autor: </b> Get Arrays
+
---
+++ src/main/java/com/magadiflo/app/SpringSecurityJwtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtBackendApplication.class, args);
+	}
+
+}
