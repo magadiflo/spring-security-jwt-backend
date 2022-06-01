4c89248638725a6f9847a0c966c1da1d5f348a4e
Martín <magadiflo@gmail.com>
2022-06-01 17:48:57 -0500

    Security constants

---
+++ src/main/java/com/magadiflo/app/constant/SecurityConstant.java
@@ -0,0 +1,17 @@
+package com.magadiflo.app.constant;
+
+public class SecurityConstant {
+
+    public static final long EXPIRATION_TIME = 432_000_000;//(5 * 24 * 60 * 60 * 1000) 5 days expressed in milliseconds
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String MAGADIFLO_LLC = "By Magadiflo company";
+    public static final String MAGADIFLO_ADMINISTRATION = "User Management Portal";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to log in to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = { "/user/login", "/user/register", "/user/reset-password/**", "/user/image/**" }; //Acceden sin estar autenticados
+
+}
