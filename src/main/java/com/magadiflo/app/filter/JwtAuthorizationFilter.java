6defbc9d45568ab88ca25675ddd6a8f1795975ee
Martín <magadiflo@gmail.com>
2022-06-02 00:35:02 -0500

    JWT Authorization Filter - Part 1

---
+++ src/main/java/com/magadiflo/app/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,41 @@
+package com.magadiflo.app.filter;
+
+import com.magadiflo.app.constant.SecurityConstant;
+import com.magadiflo.app.utility.JWTTokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private JWTTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(JWTTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    /**
+     * Este método se disparará cada vez que llegue una solicitud,
+     * y eso solo va a suceder una vez.
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if (request.getMethod().equalsIgnoreCase(SecurityConstant.OPTIONS_HTTP_METHOD)) {
+            response.setStatus(HttpStatus.OK.value());
+        } else {
+            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+            if (authorizationHeader == null || !authorizationHeader.startsWith(SecurityConstant.TOKEN_PREFIX)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+        }
+    }
+
+}
