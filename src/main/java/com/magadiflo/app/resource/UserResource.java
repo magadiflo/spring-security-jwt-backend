8f20d0097e6dcfcd28f806e06187357ff3fa3f54
Martín <magadiflo@gmail.com>
2022-06-06 11:25:02 -0500

    Logging in and generating JWT

--- src/main/java/com/magadiflo/app/resource/UserResource.java
+++ src/main/java/com/magadiflo/app/resource/UserResource.java
@@ -1,14 +1,20 @@
 package com.magadiflo.app.resource;
 
+import com.magadiflo.app.constant.SecurityConstant;
 import com.magadiflo.app.domain.User;
+import com.magadiflo.app.domain.UserPrincipal;
 import com.magadiflo.app.exception.ExceptionHandling;
 import com.magadiflo.app.exception.domain.EmailExistException;
 import com.magadiflo.app.exception.domain.UserNotFoundException;
 import com.magadiflo.app.exception.domain.UsernameExistException;
 import com.magadiflo.app.service.IUserService;
+import com.magadiflo.app.utility.JWTTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -24,10 +30,14 @@ import org.springframework.web.bind.annotation.*;
 public class UserResource extends ExceptionHandling {
 
     private final IUserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final JWTTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserResource(IUserService userService) {
+    public UserResource(IUserService userService, AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/register")
@@ -37,4 +47,23 @@ public class UserResource extends ExceptionHandling {
         return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        this.authenticate(user.getUsername(), user.getPassword());
+        User loginUser = this.userService.findUserByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = this.getJwtHeader(userPrincipal);
+
+        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal user) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(SecurityConstant.JWT_TOKEN_HEADER, this.jwtTokenProvider.generateJwtToken(user));
+        return headers;
+    }
+
+    private void authenticate(String username, String password) {
+        this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    }
 }
