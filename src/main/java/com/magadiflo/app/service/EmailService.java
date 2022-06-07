841b61b0514ac7d4972ea4bcba1bd3615de81174
Martín <magadiflo@gmail.com>
2022-06-07 12:29:59 -0500

    Email content

--- src/main/java/com/magadiflo/app/service/EmailService.java
+++ src/main/java/com/magadiflo/app/service/EmailService.java
@@ -2,7 +2,12 @@ package com.magadiflo.app.service;
 
 import static com.magadiflo.app.constant.EmailConstant.*;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
 import java.util.Properties;
 
 public class EmailService {
@@ -15,6 +20,18 @@ public class EmailService {
         properties.put(SMTP_STARTTLS_ENABLE, true);
         properties.put(SMTP_STARTTLS_REQUIRED, true);
         return Session.getInstance(properties, null);
+    }
+
+    private Message createEmail(String firstName, String password, String email) throws MessagingException {
+        Message message = new MimeMessage(this.getEmailSession());
+        message.setFrom(new InternetAddress(FROM_EMAIL));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(CC_EMAIL, false));
+        message.setSubject(EMAIL_SUBJECT);
+        message.setText("Hello ".concat(firstName).concat(",\n\nYour new account password is: ").concat(password).concat("\n\nThe Support Team."));
+        message.setSentDate(new Date());
+        message.saveChanges();
+        return message;
     }
 
 }
