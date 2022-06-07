27cf611158e833446f67e323d9879622ef61c2d0
Martín <magadiflo@gmail.com>
2022-06-06 19:04:46 -0500

    Brute force attack caché - Part 1 - Configurando Caché con Guava de Google

---
+++ src/main/java/com/magadiflo/app/service/LoginAttemptService.java
@@ -0,0 +1,33 @@
+package com.magadiflo.app.service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Configuración usando librería guava de google
+ * <a href="https://github.com/google/guava/wiki/CachesExplained">Google/Guava</a>
+ */
+
+@Service
+public class LoginAttemptService {
+
+    private static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
+    private static final int ATTEMPT_INCREMENT = 1;
+    private LoadingCache<String, Integer> loginAttemptCache;
+
+    public LoginAttemptService() {
+        super();
+        this.loginAttemptCache = CacheBuilder.newBuilder().expireAfterWrite(15, TimeUnit.MINUTES)
+                .maximumSize(100).build(new CacheLoader<String, Integer>() {
+                    @Override
+                    public Integer load(String key) throws Exception {
+                        return 0;
+                    }
+                });
+    }
+
+}
