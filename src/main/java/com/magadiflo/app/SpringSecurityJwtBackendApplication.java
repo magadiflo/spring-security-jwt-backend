ecfdf1db5e7902c15f33169ff6113d149a558dc8
Martín <magadiflo@gmail.com>
2022-06-07 17:49:02 -0500

    File constant

--- src/main/java/com/magadiflo/app/SpringSecurityJwtBackendApplication.java
+++ src/main/java/com/magadiflo/app/SpringSecurityJwtBackendApplication.java
@@ -1,15 +1,22 @@
 package com.magadiflo.app;
 
+import com.magadiflo.app.constant.FileConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.io.File;
 
 @SpringBootApplication
 public class SpringSecurityJwtBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityJwtBackendApplication.class, args);
+
+		//Creando el directorio donde se almacenarán las imágenes
+		//En el caso de mi pc sería: C:\Users\USUARIO\supportportal\user
+		new File(FileConstant.USER_FOLDER).mkdirs();
 	}
 
 	@Bean
