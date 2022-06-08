0e8f449947edbe18c95d1d81624214f5924cfa13
Martín <magadiflo@gmail.com>
2022-06-08 17:36:04 -0500

    Get user temporary profile image

--- src/main/java/com/magadiflo/app/resource/UserResource.java
+++ src/main/java/com/magadiflo/app/resource/UserResource.java
@@ -23,7 +23,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -149,8 +152,22 @@ public class UserResource extends ExceptionHandling {
     }
 
     @GetMapping(path = "/image/{username}/{filename}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public byte[] getImageProfile(@PathVariable String username, @PathVariable String filename) throws IOException {
+    public byte[] getProfileImage(@PathVariable String username, @PathVariable String filename) throws IOException {
         return Files.readAllBytes(Paths.get(FileConstant.USER_FOLDER + username + FileConstant.FORWARD_SLASH + filename));
+    }
+
+    @GetMapping(path = "/image/profile/{username}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable String username) throws IOException {
+        URL url = new URL(FileConstant.TEMP_PROFILE_IMAGE_BASE_URL + username);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int bytesRead;
+            byte[] chunk = new byte[1024];
+            while ((bytesRead = inputStream.read(chunk)) > 0) {
+                byteArrayOutputStream.write(chunk, 0, bytesRead);
+            }
+        }
+        return byteArrayOutputStream.toByteArray();
     }
 
     private HttpHeaders getJwtHeader(UserPrincipal user) {
